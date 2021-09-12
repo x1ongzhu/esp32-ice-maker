d9b39019286563e59f3c2ced59ca2a7d220e0584
xiongzhu <xz692949348>
2021-09-13 00:40:01 +0800

    first commit

---
+++ main/mqtt.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+esp_mqtt_client_handle_t mqtt_client;
+
+void setup_mqtt(void);
